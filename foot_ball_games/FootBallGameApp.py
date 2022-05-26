d8aae2f3eeb14068ed39fc7fd7ee42c1220a5526
Frelin AMPILAHY <randofrils@gmail.com>
2022-05-26 09:08:55 +0200

    update code

--- foot_ball_games/FootBallGameApp.py
+++ foot_ball_games/FootBallGameApp.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     spark_session = SparkSession.builder.appName("foot-ball-games").master(
         "local").getOrCreate()
 
-    foot_ball_games_DF = spark_session.read.schema(Schemas.FootBallGame.foot_ball_game) \
+    foot_ball_games_DF = spark_session.read.schema(Schemas.FootBallGame.schema) \
         .option("mode", "permissive") \
         .option("header", True).csv(f"..{SEPARATOR}files{SEPARATOR}foot-ball-games.csv")
 
--- foot_ball_games/Schemas.py
+++ foot_ball_games/Schemas.py
@@ -1,20 +1,19 @@
 from pyspark.sql.types import StructType, IntegerType, StringType, BooleanType, StructField, DataType
 
 
+def int_field(field_name: str, nullable: bool) -> StructField:
+    return StructField(field_name, IntegerType(), nullable)
+
+
+def string_field(field_name: str, nullable: bool) -> StructField:
+    return StructField(field_name, StringType(), nullable)
+
+
+def bool_field(field_name: bool, nullable: bool) -> StructField:
+    return StructField(field_name, BooleanType(), nullable)
+
+
 class FootBallGame(object):
-
-    @staticmethod
-    def int_field(field_name: str, nullable: bool) -> StructField:
-        return StructField(field_name, IntegerType(), nullable)
-
-    @staticmethod
-    def string_field(field_name: str, nullable: bool) -> StructField:
-        return StructField(field_name, StringType(), nullable)
-
-    @staticmethod
-    def bool_field(field_name: bool, nullable: bool) -> StructField:
-        return StructField(field_name, BooleanType(), nullable)
-
     _date: str = string_field("date", False)
     _home_team: str = string_field("home_team", False)
     _away_team: str = string_field("away_team", False)
@@ -25,5 +24,5 @@ class FootBallGame(object):
     _country: str = string_field("country", False)
     _neutral: bool = bool_field("neutral", False)
 
-    foot_ball_game = StructType(
+    schema = StructType(
         [_date, _home_team, _away_team, _home_score, _away_score, _tournament, _city, _country, _neutral])
