73f41425d02bca85c9f8d21f7cc6e48e587e773f
Frelin AMPILAHY <randofrils@gmail.com>
2022-05-27 13:20:08 +0200

    update code

--- foot_ball_games/Conf.py
+++ foot_ball_games/Conf.py
@@ -1,13 +1,18 @@
+import os
 from typing import List
+
+SEPARATOR = os.path.sep
 
 
 class Env:
-    def __init__(self, selected_country: str, extension: str, year: str):
+    def __init__(self, input_path: str, output_path: str, selected_country: str, extension: str, year: str):
+        self.input_path = input_path if input_path != "local" else f"..{SEPARATOR}files{SEPARATOR}foot-ball-games.csv"
+        self.output_path = output_path if output_path != "local" else f"..{SEPARATOR}files{SEPARATOR}"
         self.selected_country = selected_country
         self.extension = extension
         self.year = year
 
 
 def from_spark_submit(args: List[str]) -> Env:
-    env = Env(args[1], args[2], args[3])
+    env = Env(args[1], args[2], args[3], args[4], args[5])
     return env
--- foot_ball_games/FootBallGameApp.py
+++ foot_ball_games/FootBallGameApp.py
@@ -4,17 +4,15 @@ import sys
 from pyspark.sql import SparkSession, DataFrame
 
 import Schemas
-from Conf import from_spark_submit
+from Conf import from_spark_submit, SEPARATOR
 
 from Filters import filter_by_country, filter_by_year
 from OutputMapper import to_output_game_result
 
-SEPARATOR = os.path.sep
-
 if __name__ == "__main__":
-    # test = ["0", "Mexico", "MX", "2000"]
-    # conf = from_spark_submit(test)
-    conf = from_spark_submit(sys.argv)
+    test = ["0", "local", "local", "Mexico", "MX", "2000"]
+    conf = from_spark_submit(test)
+    # conf = from_spark_submit(sys.argv)
 
     spark_session: SparkSession = SparkSession.builder.appName("foot-ball-games") \
         .master("local") \
@@ -22,11 +20,11 @@ if __name__ == "__main__":
 
     foot_ball_games_DF: DataFrame = spark_session.read.schema(Schemas.FootBallGame.schema) \
         .option("mode", "permissive") \
-        .option("header", True).csv(f"..{SEPARATOR}files{SEPARATOR}foot-ball-games.csv")
+        .option("header", True).csv(conf.input_path)
 
     selected_country_games_DF: DataFrame = foot_ball_games_DF.filter(filter_by_country(conf.selected_country)) \
         .filter(filter_by_year(conf.year)).rdd.map(lambda row: to_output_game_result(row, conf.selected_country)) \
         .toDF(Schemas.OutputCountryGame.schema_list)
 
     selected_country_games_DF.write.mode("overwrite").json(
-        f"..{SEPARATOR}files{SEPARATOR}result{SEPARATOR}{conf.extension}{SEPARATOR}{conf.year}")
+        f"{conf.output_path}result{SEPARATOR}{conf.extension}{SEPARATOR}{conf.year}")
