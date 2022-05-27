7fd2e432c04f5e532f72c9c9261ff6d8fb3844d6
Frelin AMPILAHY <randofrils@gmail.com>
2022-05-27 10:32:10 +0200

    test

--- foot_ball_games/Conf.py
+++ foot_ball_games/Conf.py
@@ -2,10 +2,10 @@ from typing import List
 
 
 class Env:
-    def __init__(self, selected_country: str, year: str, extension: str):
+    def __init__(self, selected_country: str, extension: str, year: str):
         self.selected_country = selected_country
-        self.year = year
         self.extension = extension
+        self.year = year
 
 
 def from_spark_submit(args: List[str]) -> Env:
